dc96d8548ff23a620a39b03a3579c474f49472c6
sarafasraoui <fasraouisara07@gmail.com>
2026-03-31 15:29:12 +0100

    ajouter la premier partie html et tailwind css

---
+++ data.js
@@ -0,0 +1,57 @@
+// data.js
+export const games = [
+    {
+        id: 1,
+        title: "Cyberpunk 2077",
+        price: 59.99,
+        category: "RPG",
+        // Image de ville futuriste nÃ©on
+        image: "https://cdn1.epicgames.com/offer/77f2b98e2cef40c8a7437518bf420e47/EGS_Cyberpunk2077PhantomLiberty_CDPROJEKTRED_DLC_S1_2560x1440-c62f1eb1498aaea2fc109b7aa50279a3?resize=1&w=480&h=270&quality=medium",
+        description: "Plongez dans l'avenir sombre de Night City en tant que mercenaire hors-la-loi Ã  la recherche d'un implant unique."
+    },
+    {
+        id: 2,
+        title: "Elden Ring",
+        price: 49.99,
+        category: "Action",
+        // Image de chÃ¢teau/fantasy
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/119133?resize=1&w=360&h=480&quality=medium",
+        description: "Explorez l'Entre-terre, un vaste monde fantastique et devenez le prochain Seigneur d'Elden."
+    },
+    {
+        id: 3,
+        title: "Modern Warfare III",
+        price: 69.99,
+        category: "FPS",
+        // Image de soldat/Ã©quipement tactique
+        image: "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/store/games/mw3/overview/Store_GamesPDP_Hero01.png?imwidth=1920",
+        description: "Le combat contre la menace ultime continue dans cette expÃ©rience multijoueur lÃ©gendaire."
+    },
+    {
+        id: 4,
+        title: "The Witcher 3",
+        price: 29.99,
+        category: "RPG",
+        // Image de forÃªt/aventure
+        image: "https://cdn1.epicgames.com/offer/14ee004dadc142faaaece5a6270fb628/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S2_1200x1600-53a8fb2c0201cd8aea410f2a049aba3f?resize=1&w=360&h=480&quality=medium",
+        description: "Incarnez Geralt de Riv et parcourez un monde dÃ©vastÃ© par la guerre Ã  la recherche de Ciri."
+    },
+    {
+        id: 5,
+        title: "FIFA 25",
+        price: 79.99,
+        category: "Sport",
+        // Image de stade de foot
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/314499?resize=1&w=360&h=480&quality=medium",
+        description: "Vivez l'expÃ©rience de football la plus rÃ©aliste avec les plus grandes compÃ©titions mondiales."
+    },
+    {
+        id: 6,
+        title: "Starfield",
+        price: 55.00,
+        category: "RPG",
+        // Image de l'espace/astronaute
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/96437?resize=1&w=360&h=480&quality=medium",
+        description: "Explorez les Ã©toiles et dÃ©couvrez le plus grand mystÃ¨re de l'humanitÃ© dans ce RPG spatial."
+    }
+];
