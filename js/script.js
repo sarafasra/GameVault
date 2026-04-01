ff7c699d13510f8b33eaff5564a7526cce71ec53
sarafasra <sarafasraoui75@gmail.com>
2026-04-01 10:50:28 +0100

    travail sur la barre de recherche

--- js/script.js
+++ js/script.js
@@ -25,3 +25,32 @@ function afficherTousLesGames() {
 }
 
 afficherTousLesGames();
+
+
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("input", function () {
+  const value = searchInput.value.toLowerCase();
+  container.innerHTML = "";
+
+  for (let i = 0; i < games.length; i++) {
+    const game = games[i];
+
+    if (game.title.toLowerCase().includes(value)) {
+      const div = document.createElement("div");
+      div.classList.add("bg-white", "rounded-xl", "shadow", "p-3");
+
+      div.innerHTML = `
+        <img src="${game.image}" class="w-full h-40 object-cover rounded-lg" />
+        <h3 class="font-bold mt-2">${game.title}</h3>
+        <p>${game.description}</p>
+        <div>
+          <p>4.8⭐</p>
+          <p class="text-black-600 font-bold">${game.price}$</p>
+        </div>
+      `;
+
+      container.appendChild(div);
+    }
+  }
+});
