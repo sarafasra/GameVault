0abd26e09bf9f0e150bdd0104afc34354e5900ce
sarafasra <sarafasraoui75@gmail.com>
2026-04-03 16:29:35 +0100

    cree un button de dimunuer la quantité

--- js/panier.js
+++ js/panier.js
@@ -26,7 +26,7 @@ function afficherPanier() {
             </div>
             <div class="flex gap-2">
                 <button class="plus text-black px-2  font-boldrounded" data-id="${game.id}">+</button>
-
+                <button class="minus text-black font-bold px-2 rounded" data-id="${game.id}">-</button>
             </div>
         `;
 
