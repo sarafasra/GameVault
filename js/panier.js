fb92740499aa540d6b2cb2b3692c2840c73e6959
sarafasra <sarafasraoui75@gmail.com>
2026-04-03 16:13:22 +0100

    récupérer le panier depuis localStorage

--- js/panier.js
+++ js/panier.js
@@ -1,4 +1,5 @@
-
+// récupérer le panier depuis localStorage
+let panier = JSON.parse(localStorage.getItem("panier")) || [];
 
 const panierContainer = document.getElementById("panier-container");
 const totalEl = document.getElementById("total");
