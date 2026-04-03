419e082e0a3636caaa114227b6e0cc61a8874c38
sarafasra <sarafasraoui75@gmail.com>
2026-04-03 16:15:43 +0100

    ajouter une fonction pour afficher le panier

--- js/panier.js
+++ js/panier.js
@@ -4,3 +4,21 @@ let panier = JSON.parse(localStorage.getItem("panier")) || [];
 const panierContainer = document.getElementById("panier-container");
 const totalEl = document.getElementById("total");
 const commanderBtn = document.getElementById("commander");
+
+// fonction pour afficher le panier
+function afficherPanier() {
+    panierContainer.innerHTML = "";
+    let total = 0;
+
+    for (let i = 0; i < panier.length; i++) {
+        const game = panier[i];
+        total += game.price * game.quantity;
+
+        const div = document.createElement("div");
+        div.classList.add("bg-white", "p-3", "rounded-xl", "flex", "justify-between", "items-center");
+
+        panierContainer.appendChild(div);
+    }
+
+    totalEl.textContent = total.toFixed(2);
+}
