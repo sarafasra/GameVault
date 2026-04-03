0dc2a4c83755db1f328f5e1a23a41f277b44b0b6
sarafasra <sarafasraoui75@gmail.com>
2026-04-03 16:10:55 +0100

    récupération des éléments DOM du panier

---
+++ js/panier.js
@@ -0,0 +1,5 @@
+
+
+const panierContainer = document.getElementById("panier-container");
+const totalEl = document.getElementById("total");
+const commanderBtn = document.getElementById("commander");
